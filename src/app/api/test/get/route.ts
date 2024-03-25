5f9fbc46a63107e9919302a3c4ff923b0d886ad5
stronggeniusa@gmail.com <stronggeniusa@gmail.com>
2024-03-25 12:53:24 +0700

    Обновлен next 14

--- src/app/api/test/get/route.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { NextResponse } from 'next/server'
-
-export async function GET(request: Request) {
-    return NextResponse.json('Ура !')
-}
-
-export const dynamic = 'force-dynamic';
